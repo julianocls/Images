2014b141d74a736077fa5a0a3a28aa2efe1e1fd5
Juliano C L Santos <foxports@hotmail.com>
2022-05-15 00:43:55 -0300

    Initial Commit

---
+++ Images/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Images
+//
+//  Created by Juliano Santos on 15/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
